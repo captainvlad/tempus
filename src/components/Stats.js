f2383d6a308909e2b82dcd872578d1fafa2389fa
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-05-17 20:16:41 +0300

    Hw 3 added

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,10 +6,13 @@ import Footer from "./Footer";
 class Stats extends React.Component{
     render() {
         return(
-            <div id="stats-page">
-                <HeadLine />
-                <div id="stats-text">
-                    The page is yet to be done.
+            <div>
+                <div id="stats-page">
+                    <HeadLine />
+                    <h1 id="staty">Stats</h1>
+                    <div id="stats-text">
+                        The page is yet to be done.
+                    </div>
                 </div>
                 <Footer/>
             </div>
--- src/components/Tracking.js
+++ src/components/Tracking.js
@@ -1,20 +1,46 @@
 import React from "react";
-import "./styles/Tracking.css"
 import HeadLine from "./HeadLine";
 import Footer from "./Footer";
 
-class Tracking extends React.Component{
-    render() {
+class Tracking extends React.Component {
+    state = {
+        numChildren: 0
+    }
+
+    render () {
+        const children = [];
+
+        for (var i = 0; i < this.state.numChildren; i += 1) {
+            children.push(<HeadLine />);
+        };
+
         return (
-            <div id="tracking-page">
-                <HeadLine />
-                <div id="tracking-text">
-                    The page is yet to be done.
-                </div>
-                <Footer/>
-            </div>
+            <ParentComponent addChild={this.onAddChild}>
+                {children}
+            </ParentComponent>
         );
     }
+
+    onAddChild = () => {
+        this.setState({
+            numChildren: this.state.numChildren + 1
+        });
+    }
 }
+
+const ParentComponent = props => (
+    <div>
+        <HeadLine />
+        <div className="card calculator">
+            <button type="button" onClick={props.addChild} >Add component</button>
+            <div id="children-pane">
+                {props.children}
+            </div>
+        </div>
+        <Footer />
+    </div>
+);
+
+const ChildComponent = props => <div>{"I am child " + props.number}</div>;
 
 export default Tracking;
